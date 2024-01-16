940cbc422025d6789fef841330b64e3cf1bd26d4
Jared Cooper <152523733+autoimmunexd@users.noreply.github.com>
2024-01-16 16:11:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,18 +24,32 @@ class LargeInputForm(FlaskForm):
     submit_button = SubmitField('Submit')
 #best, 320, 192, 128
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = LargeInputForm()
     #if you click the button
     if form.validate_on_submit():
-        print('clicked!')
         selected_quality = form.select_field_quality.data
         selected_format = form.select_field_format.data
-        data = form.url_input_string.data
+        data = form.url_input_string.data.lower()
+        #identify the input
+        #match data:
+        #    case ""
+        
+        #sanizize the url
+        #cleaned = data.split('&')[0].strip().split('=')[1]
+
+        try:
+            command = f'youtube-dl --extract-audio --audio-format mp3 --audio-quality best {data}'
+        except:
+            print('Error')
+        
+        
+        #youtube-dl --extract-audio --audio-format mp3 --audio-quality 320K <YouTube-Video-URL>
+        #youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]' <YouTube-Video-URL>
         #differentiate between channel, playlist, or single video
-        #https://www.youtube.com/watch?v=5mpqhtK0NuU&list=PLCg4BmjNch8hKt_6BzvThMe-njUhFEM02 VIDEO
+        #https://www.youtube.com/watch?v=5mpqhtK0NuU&list=PLCg4BmjNch8hKt_6BzvThMe-njUhFEM02 VIDEO WITH LIST
+        #https://www.youtube.com/watch?v=5mpqhtK0NuU VIDEO
         #https://www.youtube.com/@HDgreenstudio/featured CHANNEL HOME
         #https://www.youtube.com/@HDgreenstudio/videos CHANNEL VIDEOS
         #https://www.youtube.com/playlist?list=PLAJMBElONP_FLESzq42kIIxM2c8jg_beQ PLAYLIST
